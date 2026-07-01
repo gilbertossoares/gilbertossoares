c59d6a98ea755f92f921f54b8de1043184b4f7a3
Gilberto Soares <gilbertossoares@gmail.com>
2026-06-30 23:25:03 -0300

    Fix internal links for GitHub Pages base path

--- radiant-radiation/astro.config.mjs
+++ radiant-radiation/astro.config.mjs
@@ -8,7 +8,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://gilbertossoares.github.io/gilbertossoares',
+	site: 'https://gilbertossoares.github.io',
+	base: '/gilbertossoares',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
